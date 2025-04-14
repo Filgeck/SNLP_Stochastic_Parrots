8ab140e6aa90454b642a30393b19d54f13743d5d
Hadi-Khan-Projects <hadi.khan.21@ucl.ac.uk>
2025-04-14 01:15:23 +0100

    :bug: Added AgentProgrammer to __init__.py imports

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,6 +3,7 @@ from .agent_basic import AgentBasic
 from .agent_file_selector import AgentFileSelector
 from .agent_file_retriever import AgentFileRetriever
 from .agent_example_retriever import AgentExampleRetriever
+from .agent_programmer import AgentProgrammer
 
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "AgentFileSelector",
     "AgentFileRetriever",
     "AgentExampleRetriever"
+    "AgentProgrammer",
 ]
